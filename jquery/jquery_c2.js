1944d83b06f562ec0e699f43051a85326c659365
vcorrea3x2 <60953533+vcorrea3x2@users.noreply.github.com>
2020-04-20 19:28:18 -0500

    Libreria para recomocimiento de Jquery

---
+++ jquery/jquery_c2.js
@@ -0,0 +1,15 @@
+/*$(Document).ready(function()){
+    alert("jquery se cargo completamente");
+})
+
+$(Document).ready(function() {
+  $("a").click(function(Envent)) {
+    alert("Gracias por todo");
+  });
+});*/
+
+$(function(){
+  $("#documento_c2").click(function(){;
+  $("#section_c2").load("video_curso_2.html");
+});
+});
